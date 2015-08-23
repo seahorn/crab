1374764d677df1406fec3e9b66aeb99a4e98001c
jorge <navasjorgea@gmail.com>
2015-08-22 17:16:41 -0700

    [IMPROVEMENT] added a template parameter to decide how to merge same key with different values.

--- include/ikos/common/mergeable_map.hpp
+++ include/ikos/common/mergeable_map.hpp
@@ -12,7 +12,36 @@
 
 namespace ikos {
 
-template < typename Key, typename Value >
+template<typename Key, typename Value>
+class merge_op_check_equal: public patricia_tree< Key, Value >::binary_op_t {
+  boost::optional< Value > apply(Value x, Value y) 
+  {
+    if (x == y)
+      return x;
+    else
+      IKOS_ERROR("mergeable_map: merging a key with two different values");
+  };
+  bool default_is_absorbing() { return false; }
+}; 
+
+template<typename Key, typename Value>
+class merge_op_first: public patricia_tree< Key, Value >::binary_op_t {
+  boost::optional< Value > apply(Value x, Value y)  {
+    return x;
+  };
+  bool default_is_absorbing() { return false; } 
+}; 
+
+template<typename Key, typename Value>
+class merge_op_second: public patricia_tree< Key, Value >::binary_op_t {  
+  boost::optional< Value > apply(Value x, Value y)  {
+    return y;
+  };
+  bool default_is_absorbing() { return false; }
+}; 
+
+template < typename Key, typename Value, 
+           typename MergeOp = merge_op_check_equal <Key, Value> >
 class mergeable_map: public writeable {
 
  private:
@@ -25,89 +54,68 @@ class mergeable_map: public writeable {
   
  private:
   patricia_tree_t _tree;
-  
- private:
-
-  class union_op: public binary_op_t {
-    
-    boost::optional< Value > apply(Value x, Value y) 
-    {
-      if (x == y)
-        return boost::optional< Value >(x);
-      else
-        throw error("mergeable_map: merging a key with two different values");
-    };
-
-    bool default_is_absorbing() 
-    {
-      return false;
-    }
-
-  }; // class union_op
-  
- private:
-
+     
   static patricia_tree_t do_union(patricia_tree_t t1, patricia_tree_t t2) {
-    union_op o;
+    MergeOp o;
     t1.merge_with(t2, o);
     return t1;
   }
   
- private:
   mergeable_map(patricia_tree_t t): _tree(t) { }
   
  public:
+
   mergeable_map(): _tree(patricia_tree_t()) { }
   
   mergeable_map(const mergeable_map_t& e): writeable(), _tree(e._tree) { }
   
   mergeable_map_t& operator=(mergeable_map_t e) {
-    this->_tree = e._tree;
+    _tree = e._tree;
     return *this;
   }
   
   iterator begin() {
-    return this->_tree.begin();
+    return _tree.begin();
   }
   
   iterator end() {
-    return this->_tree.end();
+    return _tree.end();
   }
   
   std::size_t size(){
-    return this->_tree.size();
+    return _tree.size();
   }
   
   mergeable_map_t operator|(mergeable_map_t e) {
-    mergeable_map_t u(do_union(this->_tree, e._tree));
+    mergeable_map_t u(do_union(_tree, e._tree));
     return u;
   }
   
   void set(Key k, Value v) {
-    this->_tree.insert(k, v);
+    _tree.insert(k, v);
   }
   
   mergeable_map_t& operator-=(Key k) {
-    this->_tree.remove(k);
+    _tree.remove(k);
     return *this;
   }
   
   boost::optional<Value> operator[](Key k) {
-    return this->_tree.lookup(k);
+    return _tree.lookup(k);
   }
 
   void clear() {
-    this->_tree = patricia_tree_t();
+    _tree = patricia_tree_t();
   }
   
   std::ostream& write(std::ostream& o) {
     o << "{";
-    for (typename patricia_tree_t::iterator it = this->_tree.begin(); it != this->_tree.end(); ) {
+    for (auto it = _tree.begin(); it != _tree.end(); ) {
       it->first.write(o);
       o << " -> ";
       o << it->second;
       ++it;
-      if (it != this->_tree.end()) {
+      if (it != _tree.end()) {
         o << "; ";
       }
     }
