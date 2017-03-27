bfca1fa2a80501fc04c63bec9d9f2df4daf77ad9
caballa <navasjorgea@gmail.com>
2017-03-27 09:43:44 -0700

    [FIXED] if the vertex type is a pointer then null_vertex () returns a null pointer. Before it was an undefined pointer.

--- include/crab/cg/cg_bgl.hpp
+++ include/crab/cg/cg_bgl.hpp
@@ -30,9 +30,17 @@ namespace boost {
     typedef size_t edges_size_type;
     typedef size_t degree_size_type;
 
-    static vertex_descriptor null_vertex() { 
-      vertex_descriptor n;
-      return n; 
+    static vertex_descriptor null_vertex() {
+      if (std::is_pointer<vertex_descriptor>::value)
+	return nullptr;
+      else {
+	// XXX: if vertex_descriptor is a basic type then
+	// null_vertex will return an undefined value, otherwise it
+	// will return the result of calling the default
+	// constructor.	   
+	vertex_descriptor n;
+	return n;
+      }
     }    
   }; // end class graph_traits
 
