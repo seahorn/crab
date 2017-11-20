23e5626fc7e707a3f1a03bf85a96a75c27264b50
caballa <navasjorgea@gmail.com>
2017-11-19 20:28:46 -0800

    [FIXED] out-of-line definition error with Apple LLVM 8.0.0

--- lib/debug.cpp
+++ lib/debug.cpp
@@ -2,35 +2,27 @@
 
 #ifndef NCRABLOG
 #include <set>
-using namespace crab;
-bool crab::CrabLogFlag = false;
-std::set<std::string> crab::CrabLog;
 
-void crab::CrabEnableLog (std::string x) 
-{
-  if (x.empty ()) return;
-  CrabLogFlag = true;
-  CrabLog.insert (x); 
+namespace crab {
+  bool CrabLogFlag = false;
+  std::set<std::string> CrabLog;
+
+  void CrabEnableLog (std::string x)  {
+    if (x.empty ()) return;
+    CrabLogFlag = true;
+    CrabLog.insert (x);   
+  }
 }
-
-namespace crab
-{
-  // struct LogOpt
-  // { void operator=(const std::string &tag) const 
-  //   { 
-  //     CrabEnableLog (tag); 
-  //   } 
-  // };  
-
-  // LogOpt loc;
-
-} // end namespace crab
+  
 
 #else
-void crab::CrabEnableLog (std::string x) { }
+namespace crab {
+  void CrabEnableLog (std::string x) { }
+}
 #endif
 
-using namespace crab;
-unsigned crab::CrabVerbosity = 0;
-void crab::CrabEnableVerbosity (unsigned v) { CrabVerbosity=v;}
+namespace crab {
+  unsigned CrabVerbosity = 0;
+  void CrabEnableVerbosity (unsigned v) { CrabVerbosity=v;}
+}
 
