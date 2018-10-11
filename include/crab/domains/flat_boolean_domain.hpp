09998eb94d5693104d46bc53fee35149207073b2
caballa <navasjorgea@gmail.com>
2018-10-10 23:35:07 -0700

    Fix operator+= in the flat boolean domain.
    We were not identifying boolean constraints in all cases.
    Nevertheless, we have disabled this code for now. Although, it seems
    correct it's not efficient.

--- include/crab/domains/flat_boolean_domain.hpp
+++ include/crab/domains/flat_boolean_domain.hpp
@@ -949,7 +949,7 @@ namespace crab {
       }
       
       void operator+=(linear_constraint_system_t csts) {
-	#if 0
+	#if 1
 	_product += csts;
 	for (auto v: csts.variables()) { _unchanged_vars -= v;}
 	#else
@@ -959,50 +959,50 @@ namespace crab {
 	// csts only contains non-boolean constraints. However, this
 	// is not necessarily the case when this operation is called
 	// from elsewhere.
-	
 	if (csts.is_true()) return;
+
+	// FIXME: this is a very expensive way of doing it. No need of
+	// evaluating all constraints to figure out boolean
+	// constraints
 	
-	linear_constraint_system_t non_bool_csts;
-	for (const linear_constraint_t& cst: csts) {
-	  if (cst.is_contradiction()) {
-	    *this = bottom(); // make bottom
-	    return;
+	typedef interval_domain<number_t, varname_t> interval_domain_t;
+	typedef typename interval_domain_t::interval_t interval_t;
+	interval_domain_t intervals;
+	intervals += csts;
+	if (intervals.is_bottom()) {
+	  *this = bottom(); // make bottom
+	  return;
+	}
+
+	// -- extract boolean constraints:
+	for(typename interval_domain_t::iterator it = intervals.begin(),
+	      et = intervals.end(); it!=et; ++it) {
+	  variable_t v = it->first;
+	  interval_t i = it->second;
+	  if (v.is_bool_type()) {
+	    // boolean constraint
+	    if (boost::optional<number_t> opt_i = i.singleton()) {
+	      if (*opt_i == number_t(0)) {
+		_product.first().assume_bool(v, true /*is_negated*/);
+	      } else if (*opt_i == number_t(1)) {
+		_product.first().assume_bool(v, false /*is_negated*/);
+	      } 
+	    } 
 	  }
-	  
-	  auto variables = cst.variables();
-	  
-	  // update unchanged_vars
-	  for (auto v: variables){
-	    _unchanged_vars -= v;
-	  }	  
-	  
-	  if (std::any_of(variables.begin(), variables.end(),
-			  [](variable_t v) { return v.is_bool_type();})) {
-	    // boolean constraints
-	    typedef interval_domain<number_t, varname_t> interval_domain_t;
-	    typedef typename interval_domain_t::interval_t interval_t;
-	    interval_domain_t intervals;
-	    intervals += cst;
-	    for(typename interval_domain_t::iterator it = intervals.begin(),
-	    	  et = intervals.end(); it!=et; ++it) {
-	      variable_t v = it->first;
-	      interval_t i = it->second;
-	      if (v.is_bool_type()) {
-	      	if (boost::optional<number_t> opt_i = i.singleton()) {
-	      	  if (*opt_i == number_t(0)) {
-	      	    _product.first().assume_bool(v, true /*is_negated*/);
-	      	  } else if (*opt_i == number_t(1)) {
-	      	    _product.first().assume_bool(v, false /*is_negated*/);
-	      	  }
-	      	}
-	      }
-	    }
-	  } else {
-	    // non-boolean constraints
-	    non_bool_csts += cst;
+	}	
+	// -- extract non-boolean constraints
+	for (const linear_constraint_t& cst: csts) {
+	  auto const& variables = cst.variables();
+	  if (std::all_of(variables.begin(), variables.end(),
+			  [](const variable_t& v) { return !v.is_bool_type();})) {
+	    _product.second() += cst; 
 	  }
 	}
-	_product.second() += non_bool_csts;
+	
+	// update unchanged_vars
+	for (auto v: csts.variables()){
+	    _unchanged_vars -= v;
+	}	  
 	#endif 
       }
       
