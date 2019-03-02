3d825b0f699471076a7421b93eeeea93ebe4272e
caballa <navasjorgea@gmail.com>
2019-03-02 12:06:58 -0800

    Cleanup of stats

--- include/crab/common/stats.hpp
+++ include/crab/common/stats.hpp
@@ -1,255 +1,65 @@
 #pragma once 
 
-/* Code from SeaHorn */
-
 #include <map>
-#include <sys/time.h>
-#include <sys/resource.h>
+#include <string>
 
-#include <crab/config.h>
 #include <crab/common/types.hpp>
 
-namespace crab
-{
-
-#ifdef HAVE_STATS
-  class Stopwatch
-  {
-  private:
+namespace crab {
+  
+  class Stopwatch {
     long started;
     long finished;
     long timeElapsed;
 
-    long systemTime () const
-    {
-      struct rusage ru;
-      getrusage (RUSAGE_SELF, &ru);
-      long r = ru.ru_utime.tv_sec * 1000000L + ru.ru_utime.tv_usec;
-      return r;
-
-    }
-
+    long systemTime() const;
   public:
-    Stopwatch () { start (); }
-
-    void start ()
-    {
-      started = systemTime ();
-      finished = -1;
-      timeElapsed = 0;
-    }
-
-    void stop ()
-    {
-      if (finished < started)
-	{
-	  finished = systemTime ();
-	}
-
-    }
-
-    void resume ()
-    {
-      if (finished >= started)
-	{
-	  timeElapsed += finished - started;
-	  started = systemTime ();
-	  finished = -1;
-	}
-    }
-
-    long getTimeElapsed () const
-    {
-      if (finished < started) return timeElapsed + systemTime () - started;
-      return timeElapsed + finished - started;
-    }
-
-
-    void Print (crab_os &out) const;
-
-    double toSeconds(){
-      double time = ((double) getTimeElapsed () / 1000000) ;
-      return time;
-    }
-
+    
+    Stopwatch();
+    void start();
+    void stop();
+    void resume();
+    long getTimeElapsed() const;
+    void Print(crab_os &out) const;
+    double toSeconds();
   };
 
 
-  /** Computes running average */
-  class Averager
-  {
-  private:
-    size_t count;
-    double avg;
-
-  public :
-    Averager () : count(0), avg (0) {}
-
-    double add (double k)
-    {
-      avg += (k - avg)/(++count);
-      return avg;
-    }
-
-    void Print (crab_os &out) const;
-
-  };
-#else
-  struct Stopwatch
-  {
-    Stopwatch () {}
-    void start () {}
-    void stop () {}
-    void resume () {}
-    long getTimeElapsed () const { return 0;}
-    void Print (crab_os &out) {}
-    double toSeconds(){ return 0.0;}
-
-  };
-  struct Averager
-  {
-  public :
-   Averager () {}
-   double add (double k) { return 0.0;}
-   void Print (crab_os &out) const {}
-  };
-
-#endif 
-}
-
-
-
-namespace crab
-{
-
-#ifdef HAVE_STATS
-  inline crab_os &operator<< (crab_os &OS, const Stopwatch &sw)
-  {
-    sw.Print (OS);
+  inline crab_os &operator<<(crab_os &OS, const Stopwatch &sw) {
+    sw.Print(OS);
     return OS;
   }
 
-  inline crab_os &operator<< (crab_os &OS, const Averager &av)
-  {
-    av.Print (OS);
-    return OS;
-  }
 
-  class CrabStats
-  {
-  private:
+  class CrabStats {
     static std::map<std::string,unsigned> counters;
     static std::map<std::string,Stopwatch> sw;
-    static std::map<std::string,Averager> av;
-    static std::map<std::string,std::string> ss;
-
   public:
     static void reset();
-    static unsigned  get (const std::string &n);
-    static double avg (const std::string &n, double v);
-    static unsigned uset (const std::string &n, unsigned v);
-
-    static void sset (const std::string &n, std::string v);
-    static std::string& sget (const std::string &n);
     
-    static void count (const std::string &name);
-    static void count_max (const std::string &name, unsigned v);
+    /* counters */
+    static unsigned get(const std::string &n);
+    static unsigned uset(const std::string &n, unsigned v);
+    static void count(const std::string &name);
+    static void count_max(const std::string &name, unsigned v);
 
-    static void start (const std::string &name);
-    static void stop (const std::string &name);
-    static void resume (const std::string &name);
+    /* stop watch */
+    static void start(const std::string &name);
+    static void stop(const std::string &name);
+    static void resume(const std::string &name);
 
     /** Outputs all statistics to std output */
-    static void Print (crab_os &OS);
-    static void PrintBrunch (crab_os &OS);
-  };
-
-
-
-  /**
-      Usage: add
-         auto_timer X("foo.bar");
-      at the beginning of a block (i.e., function body, conditional, etc)
-      to measure the time it takes to execute.
-   */
-  // class auto_timer
-  // {
-  // private:
-  //   std::string n;
-  // public:
-  //   auto_timer (const std::string &name) : n(name) { CrabStats::resume (n); }
-  //   ~auto_timer () { CrabStats::stop (n); }
-  // };
-
-  template <typename Output>
-  class TimeIt
-  {
-    const char* m_msg;
-    Output &m_out;
-    Stopwatch m_sw;
-    double m_min;
-    
-  public:
-    TimeIt (const char *msg, Output out, double min = 0.0) :
-      m_msg (msg), m_out (out), m_min (min) {}
-    ~TimeIt () 
-    {
-      m_sw.stop ();
-      if (m_sw.toSeconds () >= m_min)
-        m_out << "TimeIt: " << m_msg << " " << m_sw << "\n";
-    }
-    
+    static void Print(crab_os &OS);
+    static void PrintBrunch(crab_os &OS);
   };
   
-  class ScopedCrabStats 
-  {
+  class ScopedCrabStats {
     std::string m_name;
   public:
-    ScopedCrabStats (const std::string &name, bool reset = false) : m_name(name) 
-    { 
-      if (reset) 
-        { 
-          m_name += ".last";
-          CrabStats::start (m_name);
-        }
-      else
-        CrabStats::resume (m_name); 
-    }
-    ~ScopedCrabStats () { CrabStats::stop (m_name); }
+    ScopedCrabStats(const std::string &name, bool reset = false);
+    ~ScopedCrabStats();
   };  
-#else
-  inline crab_os &operator<< (crab_os &OS, const Stopwatch &sw){ return OS;}
-  inline crab_os &operator<< (crab_os &OS, const Averager &av){ return OS;}
-  struct CrabStats
-  {
-    static void reset(){}    
-    static unsigned  get (const std::string &n){ return 0;}
-    static double avg (const std::string &n, double v){ return 0.0;}
-    static unsigned uset (const std::string &n, unsigned v){return 0;}
-    static void sset (const std::string &n, std::string v){}
-    static std::string& sget (const std::string &n)
-    { CRAB_ERROR("Stats::sget not implemented");}
-    static void count (const std::string &name){}
-    static void count_max (const std::string &name, unsigned v){}
-    static void start (const std::string &name){}
-    static void stop (const std::string &name){}
-    static void resume (const std::string &name){}
-    static void Print (crab_os &OS){}
-    static void PrintBrunch (crab_os &OS){}
-  };
-  template <typename Output>
-  struct TimeIt
-  {
-    TimeIt (const char *msg, Output out, double min = 0.0) {}
-  };
-  struct ScopedCrabStats 
-  {
-    ScopedCrabStats (const std::string &name, bool reset = false) {}
-  };  
-#endif 
 }
 
-#define CRAB_MEASURE_FN crab::ScopedCrabStats __stats__(__FUNCTION__)
-#define CRAB_MEASURE_FN_LAST crab::ScopedCrabStats __stats_last__(__FUNCTION__, true)
 
 
--- lib/stats.cpp
+++ lib/stats.cpp
@@ -1,79 +1,55 @@
-#include <crab/config.h>
+#include <crab/common/stats.hpp>
 
-#ifdef HAVE_STATS
-#include "crab/common/stats.hpp"
+#include <sys/time.h>
+#include <sys/resource.h>
 
-namespace crab
-{
+namespace crab {
+  
   std::map<std::string,unsigned> CrabStats::counters;
   std::map<std::string,Stopwatch> CrabStats::sw;
-  std::map<std::string,Averager> CrabStats::av;
-  std::map<std::string,std::string> CrabStats::ss;
 
-  void CrabStats::reset () {
-    counters.clear();
-    sw.clear();
-    av.clear();
-    ss.clear();
+  long Stopwatch::systemTime() const {
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru);
+    long r = ru.ru_utime.tv_sec * 1000000L + ru.ru_utime.tv_usec;
+    return r;
   }
 
-  void CrabStats::count (const std::string &name) { ++counters[name]; }
-  void CrabStats::count_max (const std::string &name, unsigned v) {
-      counters[name] = std::max (counters[name], v);
+  Stopwatch::Stopwatch() { start(); }
+
+  void Stopwatch::start() {
+    started = systemTime();
+    finished = -1;
+    timeElapsed = 0;
   }
 
-  double CrabStats::avg (const std::string &n, double v) { return av[n].add (v); }
-  unsigned CrabStats::uset (const std::string &n, unsigned v)
-  { return counters [n] = v; }
-  unsigned CrabStats::get (const std::string &n) { return counters [n]; }
-
-  void CrabStats::sset (const std::string &n, std::string v) {ss [n] = v;}
-  std::string& CrabStats::sget (const std::string &n) {return ss[n];}
+  void Stopwatch::stop() {
+    if (finished < started) {
+      finished = systemTime();
+    }
+  }
   
-  void CrabStats::start (const std::string &name) { sw[name].start (); }
-  void CrabStats::stop (const std::string &name) { sw[name].stop (); }
-  void CrabStats::resume (const std::string &name) { sw[name].resume (); }
-
-  /** Outputs all statistics to std output */
-  void CrabStats::Print (crab_os &OS) {
-    OS << "\n\n************** STATS ***************** \n";
-    for (auto &kv : ss)
-      OS << kv.first << ": " << kv.second << "\n";
-    for (auto &kv : counters)
-      OS << kv.first << ": " << kv.second << "\n";
-
-    for (auto &kv : sw)
-      OS << kv.first << ": " << kv.second << "\n";
-
-    for (auto &kv : av)
-      OS << kv.first << ": " << kv.second << "\n";
-
-
-    OS << "************** STATS END ***************** \n";
+  void Stopwatch::resume() {
+    if (finished >= started) {
+      timeElapsed += finished - started;
+      started = systemTime();
+      finished = -1;
+    }
+  }
+  
+  long Stopwatch::getTimeElapsed() const {
+    if (finished < started)
+      return timeElapsed + systemTime() - started;
+    else 
+      return timeElapsed + finished - started;
   }
 
-  void CrabStats::PrintBrunch (crab_os &OS)
-  {
-    OS << "\n\n************** BRUNCH STATS ***************** \n";
-    for (auto &kv : ss) 
-      OS << "BRUNCH_STAT " << kv.first << " " << kv.second << "\n";
-    
-    for (auto &kv : counters)
-      OS << "BRUNCH_STAT " << kv.first << " " << kv.second << "\n";
-
-    for (auto &kv : sw)
-      OS << "BRUNCH_STAT " << kv.first << " " 
-         << (kv.second).toSeconds() << "\n";
-
-    for (auto &kv : av)
-      OS << "BRUNCH_STAT " << kv.first << " " << kv.second << "\n";
-
-    OS << "************** BRUNCH STATS END ***************** \n";
+  double Stopwatch::toSeconds(){
+    double time = ((double) getTimeElapsed() / 1000000) ;
+    return time;
   }
 
-
-  void Stopwatch::Print (crab_os &out) const
-  {
+  void Stopwatch::Print (crab_os &out) const {
     long time = getTimeElapsed ();
     long h = time/3600000000L;
     long m = time/60000000L - h*60;
@@ -83,7 +59,56 @@ namespace crab
     if (m > 0) out << m << "m";
     out << s << "s";
   }  
+  
+  void CrabStats::reset () {
+    counters.clear();
+    sw.clear();
+  }
+
+  void CrabStats::count (const std::string &name) { ++counters[name]; }
+  void CrabStats::count_max (const std::string &name, unsigned v) {
+      counters[name] = std::max (counters[name], v);
+  }
+
+  unsigned CrabStats::uset (const std::string &n, unsigned v) 
+  { return counters [n] = v; }
+  unsigned CrabStats::get (const std::string &n) { return counters [n]; }
+  
+  void CrabStats::start (const std::string &name) { sw[name].start (); }
+  void CrabStats::stop (const std::string &name) { sw[name].stop (); }
+  void CrabStats::resume (const std::string &name) { sw[name].resume (); }
+
+  /** Outputs all statistics to std output */
+  void CrabStats::Print (crab_os &OS) {
+    OS << "\n\n************** STATS ***************** \n";
+    for (auto &kv : counters)
+      OS << kv.first << ": " << kv.second << "\n";
+    for (auto &kv : sw)
+      OS << kv.first << ": " << kv.second << "\n";
+    OS << "************** STATS END ***************** \n";
+  }
+
+  void CrabStats::PrintBrunch (crab_os &OS) {
+    OS << "\n\n************** BRUNCH STATS ***************** \n";
+    for (auto &kv : counters)
+      OS << "BRUNCH_STAT " << kv.first << " " << kv.second << "\n";
+    for (auto &kv : sw)
+      OS << "BRUNCH_STAT " << kv.first << " " << (kv.second).toSeconds() << "sec \n"; 
+    OS << "************** BRUNCH STATS END ***************** \n";
+  }
+  
+  ScopedCrabStats::ScopedCrabStats(const std::string &name, bool reset)
+    : m_name(name) {
+    if (reset) {
+      m_name += ".last";
+      CrabStats::start(m_name);
+    } else {
+      CrabStats::resume(m_name);
+    }
+  }
+  
+  ScopedCrabStats::~ScopedCrabStats() { CrabStats::stop(m_name); }
+
+  
     
-  void Averager::Print (crab_os &out) const { out << avg; }
 }
-#endif 
