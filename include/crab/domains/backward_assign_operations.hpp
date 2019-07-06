1ce382ef42ce67a4109a0053677ed30f33e25b90
caballa <navasjorgea@gmail.com>
2019-07-06 15:49:30 -0700

    Generate fresh id for temporary variable

--- include/crab/domains/backward_assign_operations.hpp
+++ include/crab/domains/backward_assign_operations.hpp
@@ -60,7 +60,7 @@ namespace crab {
 	
 	if (e.variables() >= x) {
 	  auto& vfac = x.name().get_var_factory();
-	  variable_t old_x(vfac.get(x.index()), x.get_type());
+	  variable_t old_x(vfac.get(), x.get_type());
 	  std::map<variable_t,variable_t> renaming_map;
 	  renaming_map.insert({x, old_x});
 	  linear_expression_t renamed_e = e.rename(renaming_map);
