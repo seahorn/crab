0ae9a9f1e76b8abf702319a15a7289176433bd88
caballa <navasjorgea@gmail.com>
2020-06-15 10:16:01 -0500

    refactor(checker): save useless check
      Also, print invariants before check

--- include/crab/checkers/assertion.hpp
+++ include/crab/checkers/assertion.hpp
@@ -26,9 +26,9 @@ class assert_property_checker : public property_checker<Analyzer> {
   using typename base_checker_t::assign_t;
   using typename base_checker_t::assume_t;
   using typename base_checker_t::basic_block_t;
-  using typename base_checker_t::bin_op_t;  
+  using typename base_checker_t::bin_op_t;
   using typename base_checker_t::bool_assert_t;
-  using typename base_checker_t::assert_ref_t;  
+  using typename base_checker_t::assert_ref_t;
   using typename base_checker_t::lin_cst_sys_t;
   using typename base_checker_t::lin_cst_t;
   using typename base_checker_t::lin_exp_t;
@@ -102,31 +102,7 @@ public:
           os << "Invariant: " << inv;
         }
         this->add_safe(os.str(), &s);
-      } else if (crab::domains::checker_domain_traits<abs_dom_t>::intersect(
-                     inv, cst)) {
-        crab::crab_string_os os;
-        if (this->m_verbose >= 2) {
-          os << "Property : " << cst << "\n";
-          os << "Invariant: " << inv;
-        }
-        this->add_warning(os.str(), &s);
       } else {
-        /* Instead this program:
-           x:=0;
-           y:=1;
-           if (x=34) {
-             assert(y==2);
-           }
-           Suppose due to some abstraction we have:
-            havoc(x);
-            y:=1;
-            if (x=34) {
-              assert(y==2);
-            }
-           As a result, we have inv={y=1,x=34}  and cst={y=2}
-           Note that inv does not either entail or intersect with cst.
-           However, the original program does not violate the assertion.
-        */
         crab::crab_string_os os;
         if (this->m_verbose >= 2) {
           os << "Property : " << cst << "\n";
@@ -134,6 +110,38 @@ public:
         }
         this->add_warning(os.str(), &s);
       }
+      // else if (crab::domains::checker_domain_traits<abs_dom_t>::intersect(
+      //                inv, cst)) {
+      //   crab::crab_string_os os;
+      //   if (this->m_verbose >= 2) {
+      //     os << "Property : " << cst << "\n";
+      //     os << "Invariant: " << inv;
+      //   }
+      //   this->add_warning(os.str(), &s);
+      // } else {
+      //   /* Instead this program:
+      //      x:=0;
+      //      y:=1;
+      //      if (x=34) {
+      //        assert(y==2);
+      //      }
+      //      Suppose due to some abstraction we have:
+      //       havoc(x);
+      //       y:=1;
+      //       if (x=34) {
+      //         assert(y==2);
+      //       }
+      //      As a result, we have inv={y=1,x=34}  and cst={y=2}
+      //      Note that inv does not either entail or intersect with cst.
+      //      However, the original program does not violate the assertion.
+      //   */
+      //   crab::crab_string_os os;
+      //   if (this->m_verbose >= 2) {
+      //     os << "Property : " << cst << "\n";
+      //     os << "Invariant: " << inv;
+      //   }
+      //   this->add_warning(os.str(), &s);
+      // }
     }
     s.accept(&*this->m_abs_tr); // propagate invariants to the next stmt
   }
@@ -147,8 +155,7 @@ public:
       crab::crab_string_os os;
       if (this->m_verbose >= 3) {
         os << "Property : " << s << "\n";
-        auto &inv = this->m_abs_tr->get_abs_value();
-        os << "Invariant: " << inv << "\n";
+        os << "Invariant: " << this->m_abs_tr->get_abs_value() << "\n";
         os << "Note: it was proven by the forward+backward analysis";
       }
       this->add_safe(os.str(), &s);
@@ -160,22 +167,19 @@ public:
       }
 
       abs_dom_t inv1(this->m_abs_tr->get_abs_value());
-      auto bvar = s.cond();
-      inv1.assume_bool(bvar, true /*is_negated*/);
+      inv1.assume_bool(s.cond(), true /*is_negated*/);
       if (inv1.is_bottom()) {
         crab::crab_string_os os;
         if (this->m_verbose >= 3) {
           os << "Property : " << s << "\n";
-          os << "Invariant: " << inv1;
+          os << "Invariant: " << this->m_abs_tr->get_abs_value();
         }
         this->add_safe(os.str(), &s);
       } else {
-        abs_dom_t inv2(this->m_abs_tr->get_abs_value());
-        inv2.assume_bool(bvar, false /*is_negated*/);
         crab::crab_string_os os;
         if (this->m_verbose >= 2) {
           os << "Property : " << s << "\n";
-          os << "Invariant: " << inv2;
+	  os << "Invariant: " << this->m_abs_tr->get_abs_value();	  
         }
         this->add_warning(os.str(), &s);
       }
