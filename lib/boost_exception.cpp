3d66ee3010c0adce76fb85089562d9b6fe6d5b4c
caballa <navasjorgea@gmail.com>
2020-08-21 08:50:21 -0500

    fix(boost_exception): add source_location with boost 1.73

--- lib/boost_exception.cpp
+++ lib/boost_exception.cpp
@@ -1,10 +1,18 @@
 #include <cstdlib>
 #include <exception>
+#include <boost/assert/source_location.hpp>
 #ifdef BOOST_NO_EXCEPTIONS
 namespace boost {
 void throw_exception(std::exception const &e) {
   // TBD: print error message
   std::exit(1);
 }
+
+// Starting with boost 1.73  
+void throw_exception(std::exception const &e, boost::source_location const &loc) {
+  // TBD: print error message
+  std::exit(1);
+}
+  
 } // namespace boost
 #endif
