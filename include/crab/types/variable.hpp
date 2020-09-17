32f2e43011e5382b1aef625271f8452f7ae8a8a1
caballa <navasjorgea@gmail.com>
2020-09-17 15:32:04 -0500

    feat(variable): New class variable_or_number. Represent either a variable or a typed number.

--- include/crab/types/variable.hpp
+++ include/crab/types/variable.hpp
@@ -95,6 +95,7 @@ public:
   unsigned get_integer_bitwidth() const { assert(m_kind == INT_TYPE); return m_bitwidth;}
   bool is_real() const { return m_kind == REAL_TYPE; }
   bool is_reference() const { return m_kind == REF_TYPE; }
+  bool is_scalar() const { return is_bool() || is_integer() || is_real() || is_reference(); }
   //// arrays
   bool is_bool_array() const { return m_kind == ARR_BOOL_TYPE; }  
   bool is_integer_array() const { return m_kind == ARR_INT_TYPE; }
@@ -257,12 +258,91 @@ public:
   }
 
 }; // class variable
-
+  
 /** specialization for boost::hash_combine **/
 template <typename Number, typename VariableName>
 inline size_t hash_value(const variable<Number, VariableName> &v) {
   return v.hash();
 }
+
+/* 
+ * This class represents either a variable or a **typed** number.
+ */
+template <typename Number, typename VariableName>
+class variable_or_number {
+public:
+  using variable_t = variable<Number, VariableName>;
+  using number_t = Number;
+private:
+  using this_type = variable_or_number<Number, VariableName>;
+
+  boost::optional<variable_t> m_var;
+  // A number can be a boolean, a real, an integer, or a reference.
+  std::pair<number_t, variable_type> m_num;
+
+  void check_number_type(const variable_type &ty) {
+    if (!ty.is_scalar()) {
+      CRAB_ERROR("variable_or_number supports only scalar types");
+    }
+  }
+  
+public:
+  variable_or_number(variable_t var)
+    : m_var(var), m_num(std::make_pair(0, UNK_TYPE)) {}
+  
+  variable_or_number(number_t num, variable_type num_ty)
+    : m_var(boost::none), m_num(std::make_pair(num, num_ty)) {
+    check_number_type(num_ty);
+  }
+
+  variable_or_number(const this_type& o) = default;
+  variable_or_number(this_type&& o) = default;
+  this_type &operator=(const this_type& o) = default;
+  this_type &operator=(this_type&& o) = default;    
+
+  bool is_variable() const {
+    return (m_var ? true: false);
+  }
+  
+  bool is_number() const {
+    return !is_variable();
+  }
+  
+  variable_type get_type() const {
+    if (is_variable()) {
+      return (*m_var).get_type();
+    } else {
+      return m_num.second;
+    }
+  }
+
+  variable_t get_variable() const {
+    if (!is_variable()) {
+      CRAB_ERROR("variable_or_number is not a variable");
+    }
+    return (*m_var);
+  }
+
+  number_t get_number() const {
+    if (!is_number()) {
+      CRAB_ERROR("variable_or_number is not a variable");
+    }
+    return m_num.first;
+  }
+  
+  void write(crab_os &o) const {
+    if (is_variable()) {
+      o << get_variable();
+    } else {
+      o << get_number();
+    }
+  }
+
+  friend inline crab_os &operator<<(crab_os &o, const this_type &v) {
+    v.write(o);
+    return o;
+  }
+};
 
 } // end namespace crab
 
