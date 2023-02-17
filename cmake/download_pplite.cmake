43434b82b8a8012eff25f7704b9aba1565ca47cf
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2023-02-17 09:34:06 +0100

    chore: updated download link for PPLite 0.9

--- cmake/download_pplite.cmake
+++ cmake/download_pplite.cmake
@@ -1,6 +1,6 @@
 if (CRAB_USE_PPLITE)
   ExternalProject_Add(pplite
-    URL https://github.com/ezaffanella/PPLite/raw/main/releases/pplite-0.8.tar.gz
+    URL https://github.com/ezaffanella/PPLite/raw/main/releases/pplite-0.9.tar.gz
     INSTALL_DIR ${CMAKE_BINARY_DIR}/run/pplite
     CONFIGURE_COMMAND
     ../pplite/configure --prefix <INSTALL_DIR> --enable-apron --with-gmp=${GMP_SEARCH_PATH} --with-flint=${FLINT_SEARCH_PATH} --with-apron=${APRON_ROOT}
