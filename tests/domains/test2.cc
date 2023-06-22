6437336cca2784afa6bfb0dc3592b1a6c41783d9
caballa <navasjorgea@gmail.com>
2023-06-22 12:08:07 -0600

    refactor(cfg): switch cfg simplify to non-recursive version

--- tests/domains/test2.cc
+++ tests/domains/test2.cc
@@ -26,26 +26,19 @@ z_cfg_t *prog(variable_factory_t &vfac) {
   z_basic_block_t &loop1_bb1 = cfg->insert("loop1_bb1");
   z_basic_block_t &loop1_bb1_t = cfg->insert("loop1_bb1_t");
   z_basic_block_t &loop1_bb1_f = cfg->insert("loop1_bb1_f");
-  z_basic_block_t &loop1_bb2 = cfg->insert("loop1_bb2");
-  z_basic_block_t &loop2_entry = cfg->insert("loop2_entry");
   z_basic_block_t &loop2_bb1 = cfg->insert("loop2_bb1");
   z_basic_block_t &loop2_bb1_t = cfg->insert("loop2_bb1_t");
   z_basic_block_t &loop2_bb1_f = cfg->insert("loop2_bb1_f");
-  z_basic_block_t &loop2_bb2 = cfg->insert("loop2_bb2");
   z_basic_block_t &ret = cfg->insert("ret");
 
   loop1_entry >> loop1_bb1;
   loop1_bb1 >> loop1_bb1_t;
   loop1_bb1 >> loop1_bb1_f;
-  loop1_bb1_t >> loop1_bb2;
-  loop1_bb2 >> loop1_bb1;
-  loop1_bb1_f >> loop2_entry;
-
-  loop2_entry >> loop2_bb1;
+  loop1_bb1_t >>  loop1_bb1;
+  loop1_bb1_f >> loop2_bb1;
   loop2_bb1 >> loop2_bb1_t;
   loop2_bb1 >> loop2_bb1_f;
-  loop2_bb1_t >> loop2_bb2;
-  loop2_bb2 >> loop2_bb1;
+  loop2_bb1_t >> loop2_bb1;
   loop2_bb1_f >> ret;
 
   z_var i(vfac["i"], crab::INT_TYPE, 32);
@@ -56,12 +49,13 @@ z_cfg_t *prog(variable_factory_t &vfac) {
   loop1_entry.assign(k, 30);
   loop1_bb1_t.assume(i <= 9);
   loop1_bb1_f.assume(i >= 10);
-  loop1_bb2.add(i, i, 1);
+  loop1_bb1_f.assign(j, 0);
+  
+  loop1_bb1_t .add(i, i, 1);
 
-  loop2_entry.assign(j, 0);
   loop2_bb1_t.assume(j <= 9);
+  loop2_bb1_t.add(j, j, 1);  
   loop2_bb1_f.assume(j >= 10);
-  loop2_bb2.add(j, j, 1);
   return cfg;
 }
 
@@ -72,7 +66,6 @@ int main(int argc, char **argv) {
   }
   variable_factory_t vfac;
   z_cfg_t *cfg = prog(vfac);
-  cfg->simplify();
   crab::outs() << *cfg << "\n";
 
   {
